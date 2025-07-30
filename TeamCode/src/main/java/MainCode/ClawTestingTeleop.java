c18edc4389feb56595db110a0d0b397b0bcff292
wilso <479962@bsd48.org>
2025-07-29 17:28:58 -0700

    Testing

---
+++ TeamCode/src/main/java/MainCode/ClawTestingTeleop.java
@@ -0,0 +1,45 @@
+package MainCode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp
+public class ClawTestingTeleop extends LinearOpMode {
+    private Servo claw = null;
+
+    final double CLAW_IN   = 0.44; //0.675
+    final double CLAW_OUT  = 0.2; // 1
+
+    double clawPosition = CLAW_OUT;
+
+
+    @Override
+    public void runOpMode() {
+        claw = hardwareMap.get(Servo.class, "claw");
+
+        waitForStart();
+
+        while (opModeIsActive()){
+            if(gamepad1.dpad_up){
+                clawPosition = CLAW_IN;
+            } else if(gamepad1.dpad_down){
+                clawPosition = CLAW_OUT;
+            }
+
+            if(clawPosition == CLAW_IN){
+                claw.setPosition(CLAW_IN);
+            } else if (clawPosition == CLAW_OUT){
+                claw.setPosition(CLAW_OUT);
+            }
+        }
+    }
+}
